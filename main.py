fa84a6e40eb7a3717d833d5e213b4d582f2bec37
Sourena <sourena.khanzadeh@gmail.com>
2022-04-15 00:06:12 -0400

    made   basic template

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def awake(self):
+        pass
+
+    def run(self):
+        pass
+
+    def   update(self):
+        pass
+
+    def  quit(self):
+        pygame.quit()
+        quit()
