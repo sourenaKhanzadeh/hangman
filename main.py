55aca72cd76b0fa0f02bc272e84b536cf7fccb1b
Sourena <sourena.khanzadeh@gmail.com>
2022-04-15 00:18:01 -0400

    made  the  main  window

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import pygame
 from pygame.locals import *
+from pygame.color import Color
 
 
 class Game:
+    FPS = 60
+    W = 600
+    H = 600
+
     def __init__(self):
-        pass
+        self.scene = []
+        self.screen = pygame.display.set_mode((Game.W, Game.H))
 
     def awake(self):
         pass
 
     def run(self):
-        pass
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(Game.FPS)
+            for ev in pygame.event.get():
+                if ev.type == QUIT:
+                    run = False
+            self.update()
 
-    def   update(self):
-        pass
+    def update(self):
+        pygame.display.update()
+        for gameObj in self.scene:
+            gameObj.update()
+        self.screen.fill(Color('white'))
 
-    def  quit(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
         pygame.quit()
         quit()
+
+
+if __name__ == "__main__":
+    with Game() as game:
+        game.run()
+
